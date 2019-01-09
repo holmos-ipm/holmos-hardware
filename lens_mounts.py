ad7b25615aff53cc8b59c6e241adf1896ebe6132
Tobias Beckmann <tobias.beckmann@ipm.fraunhofer.de>
2019-01-09 12:57:52 +0100

    Clamp works fine as Objective Mount; Successfully printed lens mount and objective mount like this.

--- lens_mounts.py
+++ lens_mounts.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
 
     """
     https://forscherladen.lafeo.de/opti-media-achromat-2-linser-f-99-6-mm::10-550.OAL.html
-    Durchmesser: 26,0 mm
+    Durchmesser: 26,0 mm   # wrong; measured 24.3 mm -> 1"
     Brennweite: + 99,6 mm
     
     https://forscherladen.lafeo.de/opti-media-linse-nr.5-brennweite-65-mm::10-306.OML.html
@@ -23,6 +23,9 @@ if __name__ == '__main__':
     header = "$fa = 5;"  # minimum face angle
     header += "$fs = 0.1;"  # minimum face size
 
-    for d in (26.0, 16.5):
+    scad_render_to_file(round_mount_light(20, opening_angle=None, stop_inner_diam=19),
+                        "scad/objective_mount_edmund4x_simple.scad", file_header=header)
+
+    for d in (25.4, 16.5):
         scad_render_to_file(round_mount_light(d, opening_angle=None, stop_inner_diam=d-2),
                         "scad/lens mount_d{:.1f}.scad".format(d), file_header=header)
